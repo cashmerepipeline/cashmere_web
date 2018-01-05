9eca510a476de008e785adedbc4f9520d74e5167
yes7rose <yes7rose@sina.com>
2018-01-05 09:05:41 +0000

    add templates

--- cashmere.py
+++ cashmere.py
@@ -1,18 +1,21 @@
-#!/usr/bin/env python
+#!venv/bin/python
 
-from flask import Flask
-
-app = Flask(__name__)
-
-#!flask/bin/python
 from flask import Flask, render_template
 from flask_flatpages import FlatPages
+from flask_bootstrap import Bootstrap
+
 
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
 FLATPAGES_EXTENSION = '.md'
 
-app = Flask(__name__)
+def create_bootstrap_app():
+    app = Flask(__name__)
+    Bootstrap(app)
+
+    return app
+
+app = create_bootstrap_app()
 app.config.from_object(__name__)
 flatpages = FlatPages(app)
 
@@ -25,6 +28,17 @@ def index():
 def page(path):
     page = flatpages.get_or_404(path)
     return render_template('page.html', page=page)
+
+@app.route('/pipeline/<path:path>')
+def pipeline():
+    page = flask_flatpages.get_or_404(path)
+    return render_template('page.html', page=page)
+
+@app.route('/about')
+def about():
+    pages = (p for p in flatpages if 'date' in p.meta)
+    return render_template('about.html', pages=pages)
+
 
 
 if __name__=="__main__":
