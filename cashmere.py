49a98976d5cc166f54f4ffefc0ff53125f6a5cb0
yes7rose <yes7rose@sina.com>
2018-01-11 16:29:09 +0800

    add templates

--- cashmere.py
+++ cashmere.py
@@ -4,10 +4,10 @@ from flask import Flask, render_template
 from flask_flatpages import FlatPages
 from flask_bootstrap import Bootstrap
 
-
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
 FLATPAGES_EXTENSION = '.md'
+
 
 def create_bootstrap_app():
     app = Flask(__name__)
@@ -15,32 +15,44 @@ def create_bootstrap_app():
 
     return app
 
+
 app = create_bootstrap_app()
 app.config.from_object(__name__)
 flatpages = FlatPages(app)
+
 
 @app.route('/')
 def index():
     pages = (p for p in flatpages if 'date' in p.meta)
     return render_template('index.html', pages=pages)
 
+
 @app.route('/pages/<path:path>/')
 def page(path):
     page = flatpages.get_or_404(path)
     return render_template('page.html', page=page)
 
-@app.route('/pipeline/<path:path>')
-def pipeline():
-    page = flask_flatpages.get_or_404(path)
+
+@app.route('/guideline/<path:path>/')
+def guideline(path):
+    page = flatpages.get_or_404(path)
     return render_template('page.html', page=page)
 
-@app.route('/about')
+
+@app.route('/case_demos/')
+def case_demos():
+    return render_template('case_demos.html')
+
+
+@app.route('/tutorials/')
+def tutorials():
+    return render_template('tutorials.html')
+
+
+@app.route('/about/')
 def about():
-    pages = (p for p in flatpages if 'date' in p.meta)
-    return render_template('about.html', pages=pages)
+    return render_template('about.html')
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
-
